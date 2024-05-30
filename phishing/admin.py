d7c9252fbb986039cd52e1fdabe49a669410739f
Tudor Ioan Mihail <113465589+tudormihail5@users.noreply.github.com>
2024-05-30 02:13:59 +0100

    Delete phishing directory

--- phishing/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import UrlRecord
-
-admin.site.register(UrlRecord)
--- phishing/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-
-class UrlRecord(models.Model):
-    url = models.URLField(max_length=2000)
-    result = models.CharField(max_length=25)
